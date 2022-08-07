d821d9ed35dd574f6594715a8bf993b5da47e5b7
Dakota Sexton <dakota@tinykitelab.com>
2022-08-07 16:18:36 -0500

    fix: Missing mobile margins on contact page

--- app/routes/contact.jsx
+++ app/routes/contact.jsx
@@ -3,6 +3,12 @@ import { useLoaderData } from "@remix-run/react";
 import { getClient } from "~/lib/sanity/getClient";
 import Article from "~/components/Article";
 import Form from "~/components/Form";
+import styled from "styled-components";
+
+const Main = styled.main`
+  width: 90%;
+  margin: 0 auto;
+`;
 
 export async function loader({ request, params }) {
   const query = `*[_type == "page" && slug.current == "contact"]`;
@@ -16,10 +22,10 @@ export async function loader({ request, params }) {
 const ContactPage = () => {
   let { initialData } = useLoaderData();
   return (
-    <>
+    <Main>
       <Article content={initialData[0]} />
       <Form name={"Contact"} successUrl="/thank-you/" />
-    </>
+    </Main>
   );
 };
 
