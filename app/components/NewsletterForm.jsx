f8c1fe8fd237ae13476a47d231f53e708aa94786
Dakota Sexton <dakota@tinykitelab.com>
2022-08-07 12:38:50 -0500

    fix: issues with incorrect labels

--- app/components/NewsletterForm.jsx
+++ app/components/NewsletterForm.jsx
@@ -7,19 +7,6 @@ const NewsletterContainer = styled.aside`
   justify-items: center;
 `;
 
-const NewsletterDescription = styled.p`
-  max-width: 40rem;
-  margin-top: 0.5rem;
-  font-family: "termina", sans-serif;
-  font-weight: 400;
-  font-size: 0.875rem;
-  line-height: 1.5;
-
-  @media (min-width: 50rem) {
-    font-size: 1rem;
-  }
-`;
-
 const NewsletterForm = () => {
   return (
     <NewsletterContainer>
